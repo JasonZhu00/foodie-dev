90f4d5e9ac48f8f9ef4f5c31aa609e47d51661ed
JasonZhu00 <jungang1978@gmail.com>
2021-06-02 22:44:07 +0800

    2.2 首页模块 二级分类、最新商品功能

--- foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
@@ -1,12 +1,12 @@
 package com.imooc.controller;
 
+import com.imooc.pojo.Category;
+import com.imooc.pojo.vo.CategoryVO;
+import com.imooc.pojo.vo.NewItemsVO;
+import com.imooc.service.CategoryService;
 import enums.YesOrNo;
 import com.imooc.pojo.Carousel;
-//import com.imooc.pojo.Category;
-//import com.imooc.pojo.vo.CategoryVO;
-//import com.imooc.pojo.vo.NewItemsVO;
 import com.imooc.service.CarouselService;
-//import com.imooc.service.CategoryService;
 import utils.IMOOCJSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -29,8 +29,8 @@ public class IndexController {
     @Autowired
     private CarouselService carouselService;
 
-//    @Autowired
-//    private CategoryService categoryService;
+    @Autowired
+    private CategoryService categoryService;
 
     @ApiOperation(value = "获取首页轮播图列表", notes = "获取首页轮播图列表", httpMethod = "GET")
     @GetMapping("/carousel")
@@ -44,39 +44,39 @@ public class IndexController {
      * 1. 第一次刷新主页查询大分类，渲染展示到首页
      * 2. 如果鼠标上移到大分类，则加载其子分类的内容，如果已经存在子分类，则不需要加载（懒加载）
      */
-//    @ApiOperation(value = "获取商品分类(一级分类)", notes = "获取商品分类(一级分类)", httpMethod = "GET")
-//    @GetMapping("/cats")
-//    public IMOOCJSONResult cats() {
-//        List<Category> list = categoryService.queryAllRootLevelCat();
-//        return IMOOCJSONResult.ok(list);
-//    }
+    @ApiOperation(value = "获取商品分类(一级分类)", notes = "获取商品分类(一级分类)", httpMethod = "GET")
+    @GetMapping("/cats")
+    public IMOOCJSONResult cats() {
+        List<Category> list = categoryService.queryAllRootLevelCat();
+        return IMOOCJSONResult.ok(list);
+    }
 
-//    @ApiOperation(value = "获取商品子分类", notes = "获取商品子分类", httpMethod = "GET")
-//    @GetMapping("/subCat/{rootCatId}")
-//    public IMOOCJSONResult subCat(
-//            @ApiParam(name = "rootCatId", value = "一级分类id", required = true)
-//            @PathVariable Integer rootCatId) {
-//
-//        if (rootCatId == null) {
-//            return IMOOCJSONResult.errorMsg("分类不存在");
-//        }
-//
-//        List<CategoryVO> list = categoryService.getSubCatList(rootCatId);
-//        return IMOOCJSONResult.ok(list);
-//    }
-//
-//    @ApiOperation(value = "查询每个一级分类下的最新6条商品数据", notes = "查询每个一级分类下的最新6条商品数据", httpMethod = "GET")
-//    @GetMapping("/sixNewItems/{rootCatId}")
-//    public IMOOCJSONResult sixNewItems(
-//            @ApiParam(name = "rootCatId", value = "一级分类id", required = true)
-//            @PathVariable Integer rootCatId) {
-//
-//        if (rootCatId == null) {
-//            return IMOOCJSONResult.errorMsg("分类不存在");
-//        }
-//
-//        List<NewItemsVO> list = categoryService.getSixNewItemsLazy(rootCatId);
-//        return IMOOCJSONResult.ok(list);
-//    }
+    @ApiOperation(value = "获取商品子分类", notes = "获取商品子分类", httpMethod = "GET")
+    @GetMapping("/subCat/{rootCatId}")
+    public IMOOCJSONResult subCat(
+            @ApiParam(name = "rootCatId", value = "一级分类id", required = true)
+            @PathVariable Integer rootCatId) {
+
+        if (rootCatId == null) {
+            return IMOOCJSONResult.errorMsg("分类不存在");
+        }
+
+        List<CategoryVO> list = categoryService.getSubCatList(rootCatId);
+        return IMOOCJSONResult.ok(list);
+    }
+
+    @ApiOperation(value = "查询每个一级分类下的最新6条商品数据", notes = "查询每个一级分类下的最新6条商品数据", httpMethod = "GET")
+    @GetMapping("/sixNewItems/{rootCatId}")
+    public IMOOCJSONResult sixNewItems(
+            @ApiParam(name = "rootCatId", value = "一级分类id", required = true)
+            @PathVariable Integer rootCatId) {
+
+        if (rootCatId == null) {
+            return IMOOCJSONResult.errorMsg("分类不存在");
+        }
+
+        List<NewItemsVO> list = categoryService.getSixNewItemsLazy(rootCatId);
+        return IMOOCJSONResult.ok(list);
+    }
 
 }
